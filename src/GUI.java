b748548c2776495c6d2b603fd173121be4c4b3a0
Toydotgam <MatthewJPersonalAcct@outlook.com>
2020-11-10 17:06:50 +1000

    Initial commit

---
+++ src/GUI.java
@@ -0,0 +1,79 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class GUI implements ActionListener {
+	JLabel newImagePromptText;
+	JTextField newImageFilenamePrompt;
+	JLabel oldImagePromptText;
+	JLabel oldImagePromptHintText;
+	JTextField oldImageFilenamePrompt;
+	
+	public GUI() {
+		JFrame frame = new JFrame("ABPC Lock Screen Image Changer");
+		frame.setSize(400,200);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		
+		JPanel panel = new JPanel();
+		panel.setLayout(null);
+		frame.add(panel);
+		
+		newImagePromptText = new JLabel("What's the name of the file you want to set?");
+		newImagePromptText.setBounds(10, 10, 275, 25);
+		panel.add(newImagePromptText);
+		
+		newImageFilenamePrompt = new JTextField();
+		newImageFilenamePrompt.setBounds(10, 35, 275, 25);
+		panel.add(newImageFilenamePrompt);
+		
+		oldImagePromptText = new JLabel("What's the name of the default image?");
+		oldImagePromptText.setBounds(10, 70, 275, 25);
+		panel.add(oldImagePromptText);
+		
+		oldImagePromptHintText = new JLabel("Usually \"1.jpg\", but sometimes it's different.");
+		oldImagePromptHintText.setBounds(10, 95, 275, 25);
+		panel.add(oldImagePromptHintText);
+		
+		oldImageFilenamePrompt = new JTextField();
+		oldImageFilenamePrompt.setBounds(10, 130, 275, 25);
+		panel.add(oldImageFilenamePrompt);
+		
+		JButton button = new JButton("Apply");
+		button.addActionListener(this);
+		button.setBounds(285, 10, 95, 145);
+		panel.add(button);
+		
+		frame.setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		new GUI();
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		String newFilename = newImageFilenamePrompt.getText();
+		String oldFilename = oldImageFilenamePrompt.getText();
+		
+		boolean newFilenameCorrectBoolean = newFilename.endsWith(".jpg");
+		String newFilenameCorrect = String.valueOf(newFilenameCorrectBoolean);
+		if(newFilenameCorrect == "true") {
+			System.out.println("newFilename is correct.");
+		} else {
+			System.out.println("newFilename is incorrect.");
+		}
+		
+		boolean oldFilenameCorrectBoolean = oldFilename.endsWith(".jpg");
+		String oldFilenameCorrect = String.valueOf(oldFilenameCorrectBoolean);
+		if(oldFilenameCorrect == "true") {
+			System.out.println("oldFilename is correct.");
+		} else {
+			System.out.println("oldFilename is incorrect.");
+		}
+	}
+}
